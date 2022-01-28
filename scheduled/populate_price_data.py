979a4fda18d962d5be0418ba92b11b51c51b40c9
movags95 <45894549+movags95@users.noreply.github.com>
2022-01-28 01:38:53 +0000

    Bit more logic added to price pop.

--- scheduled/populate_price_data.py
+++ scheduled/populate_price_data.py
@@ -3,9 +3,15 @@ sys.path.append(os.getcwd())
 
 import functions
 
-def main(exchange):
-    symbols = functions.db.helpers.existing_symbols(exchange)
-    functions.alpaca_helpers.populate_stock_price_daily(symbols)
+def main(exchange=None, symbol_list=None, start=None):
+    if symbol_list is None:
+        symbols = functions.db.helpers.existing_symbols(exchange)
+    else:
+        symbols = symbol_list
+    if start is None:
+        functions.alpaca_helpers.populate_stock_price_daily(symbols)
+    else: 
+        functions.alpaca_helpers.populate_stock_price_daily(symbols, From=start)
 
 if __name__ == '__main__':
-    main('NASDAQ')
+    main(symbol_list=['AAPL'], start="2021-06-01")
